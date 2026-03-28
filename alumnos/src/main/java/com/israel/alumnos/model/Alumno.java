b67d5af036d30af94b9ec90a96deee05183d674a
Israel Merlyn <merlyn142a@gmail.com>
2026-03-28 15:25:11 -0600

    feat(GlobalExceptionHandler) Agregando excepcion global para identificar errores

--- alumnos/src/main/java/com/israel/alumnos/model/Alumno.java
+++ alumnos/src/main/java/com/israel/alumnos/model/Alumno.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,12 +23,20 @@ public class Alumno {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @NotBlank(message = "El número de control es obligatorio")
+    @Size(min = 8, max = 10, message = "El número de control debe tener entre 8 y 10 caracteres")
     private String numeroControl;
+    @NotBlank(message = "El nombre no puede estar vacío")
     private String nombre;
+    @NotBlank(message = "El apellido no puede estar vacío")
     private String apellido;
+    @NotBlank(message = "El teléfono es obligatorio")
+    @Pattern(regexp = "^\\d{10}$", message = "El teléfono debe ser de exactamente 10 dígitos numéricos")
     private String telefono;
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "El formato del correo electrónico no es válido")
     private String email;
+    @NotBlank(message = "La carrera es obligatoria")
     private String carrera;
     private String imagenURL;
 
